41f3fd7c8883494b3480b57fc313bcd0b08f66f9
Pranav Tomar <pranavtom1223@gmail.com>
2020-09-02 20:31:53 +0530

    Command line in progress

---
+++ Basic/arcane_basic_main.py
@@ -0,0 +1,8 @@
+
+
+
+if __name__ == "__main__":
+    print("WELCOME TO COMMAND LINE OF ARCANE BOT")
+
+    print("THERE ARE A LOTS OF DIFFERENT FUNCTIONALITY I CAN PERFORM FOR YOU")
+    print("PLEASE GUIDE ME, AND I WILL HELP YOU!")
