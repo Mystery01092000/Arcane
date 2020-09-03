84c523cf24c688e399f1611f2b410319df5b104f
Pranav Tomar <pranavtom1223@gmail.com>
2020-09-03 12:06:52 +0530

    Now our program can record and play audio

--- Basic/arcane_basic_main.py
+++ Basic/arcane_basic_main.py
@@ -1,9 +1,11 @@
 from sys import platform
+import os
 import sounddevice as sd
 from scipy.io.wavfile import write
 from Basic.google_search import search_google
 from Basic.open_google_chrome import chrome
 from playsound import playsound
+
 
 def listener(count):
     fs = 44100  # Sample rate
@@ -11,6 +13,7 @@ def listener(count):
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
     sd.wait()  # Wait until recording is finished
     write('recording/output{}.wav'.format(count), fs, myrecording)
+
 
 def speaker(count):
     playsound("recording/output{}.wav".format(count))
@@ -21,11 +24,20 @@ if __name__ == "__main__":
     count_recording = 0
     print("THERE ARE A LOTS OF DIFFERENT FUNCTIONALITY I CAN PERFORM FOR YOU")
     print("PLEASE GUIDE ME, AND I WILL HELP YOU!")
+    parent_dir = "C:/Users/prana/PycharmProjects/Arcane/Basic"
+    directory = "recording"
+
+    path = os.path.join(parent_dir, directory)
+    
+    os.mkdir(path)
+    os.chmod(path, 777)
     platform_os = platform
     for i in range(2):
         listener(count_recording)
         speaker(count_recording)
         count_recording = count_recording + 1
-    #chrome(platform_os) #To open chrome
-    #search_google("Search what you want to search") #this command will search and open in chrome
+    # chrome(platform_os) #To open chrome
+    # search_google("Search what you want to search") #this command will search and open in chrome
 
+
+    os.remove(path)
