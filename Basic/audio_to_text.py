20458ce87db2eb8cabbbbcbc66d7578739fd375b
Pranav Tomar <pranavtom1223@gmail.com>
2020-09-24 21:14:57 +0530

    updated

--- Basic/audio_to_text.py
+++ Basic/audio_to_text.py
@@ -1,2 +1,8 @@
-def audio_to_text(audio):
-    pass
+import speech_recognition as sr
+
+def audio_to_text(aud):
+    au = sr.AudioFile("../recording/output0.wav")
+    sr.recognize_api(au)
+
+
+audio_to_text("Hello")
